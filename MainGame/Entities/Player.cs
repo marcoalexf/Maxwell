b099df4a91fcba13160652123716fb5ab893fedf
Marco Francisco <marco.francisco@stark.dk>
2025-03-21 13:32:09 +0000

    starter

---
+++ MainGame/Entities/Player.cs
@@ -0,0 +1,77 @@
+﻿using MainGame.Helpers;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
+
+namespace MainGame.Entities;
+
+public class Player(SpriteBatch spriteBatch, ContentManager contentManager, Vector2 initialPosition = default)
+{
+
+    // The reference to the AnimatedTexture for the character
+    private AnimatedTexture spriteTexture = new AnimatedTexture(Vector2.Zero, rotation, scale, depth);
+    // The rotation of the character on screen
+    private const float rotation = 0;
+    // The scale of the character, how big it is drawn
+    private const float scale = 0.5f;
+    // The draw order of the sprite
+    private const float depth = 0.5f;
+
+    // How many frames/images are included in the animation
+    private const int frames = 8;
+    // How many frames should be drawn each second, how fast does the animation run?
+    private const int framesPerSec = 10;
+
+    private Viewport viewport;
+
+    protected Vector2 InputDirection = Vector2.Zero;
+
+    public void LoadContent()
+    {
+        // "AnimatedCharacter" is the name of the sprite asset in the project.
+        spriteTexture.Load(contentManager, "CharacterSheet", frames, framesPerSec);
+    }
+
+    public void Update(GameTime gameTime)
+    {
+        // Get keyboard state
+        var keyboardState = Keyboard.GetState();
+
+        InputDirection = Vector2.Zero;
+
+        if (keyboardState.IsKeyDown(Keys.W))
+        {
+            InputDirection.Y -= 1;
+        }
+        if (keyboardState.IsKeyDown(Keys.S))
+        {
+            InputDirection.Y += 1;
+        }
+        if (keyboardState.IsKeyDown(Keys.A))
+        {
+            InputDirection.X -= 1;
+        }
+        if (keyboardState.IsKeyDown(Keys.D))
+        {
+            InputDirection.X += 1;
+        }
+
+        InputDirection.Normalize();
+
+        // Assuming you have a position field to update
+        //initialPosition += InputDirection * 20 * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+        float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
+        spriteTexture.UpdateFrame(elapsed);
+    }
+
+    public void Draw()
+    {
+        spriteBatch.Begin();
+        Debug.WriteLine(initialPosition);
+        spriteTexture.DrawFrame(spriteBatch, initialPosition);
+        spriteBatch.End();
+    }
+}
---
+++ MainGame/Game1.cs
@@ -0,0 +1,61 @@
+﻿using MainGame.Entities;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MainGame
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private Player _player;
+        private SpriteBatch _spriteBatch;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // Create a basic player
+            _player = new Player(_spriteBatch, Content, new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2));
+            _player.LoadContent();
+
+            var topLeft = new Vector2(0, 0);
+            var topRight = new Vector2(_graphics.PreferredBackBufferWidth, 0);
+            var bottomLeft = new Vector2(0, _graphics.PreferredBackBufferHeight);
+            var bottomRight = new Vector2(_graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            _player.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            // Draw the player and the opponent
+            _player.Draw();
+
+            base.Draw(gameTime);
+        }
+    }
+}
